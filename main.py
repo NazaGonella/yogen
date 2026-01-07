a2ab7548aae7d964ad282eaa7c46fbfcfaa42571
NazaGonella <nazagonella2@gmail.com>
2026-01-07 14:40:27 -0300

    build function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import markdown
+import shutil
 
 from pathlib import Path
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -8,10 +9,11 @@ from watchdog.observers import Observer
 
 from parser import parse_markdown
 
+content_path = Path("content")
+build_path = Path("build")
 
 class WatchDogHandler(FileSystemEventHandler):
     def on_modified(self, event: FileSystemEvent) -> None:
-
         mod_file_path : Path = Path(event.src_path)
         if mod_file_path.suffix == ".md":
             output : str = parse_markdown(mod_file_path)
@@ -19,18 +21,33 @@ class WatchDogHandler(FileSystemEventHandler):
             output_path : Path = mod_file_path.parent / "index.html"
             output_path.write_text(output, encoding="utf-8")
 
-def main():
-    target = "posts"
-    watch_path = Path(target)
+def build(src:Path):
+    # delete pre-existing build folder
+    if build_path.exists() and build_path.is_dir():
+        shutil.rmtree(build_path)
 
+    for item in src.rglob("*"):
+        target = build_path / item.relative_to(src)
+        if item.is_dir():
+            target.mkdir(parents=True, exist_ok=True)
+        elif item.suffix == ".md":
+            target.parent.mkdir(parents=True, exist_ok=True)
+            output : str = parse_markdown(item)
+            output_path : Path = target.parent / "index.html"
+            output_path.write_text(output, encoding="utf-8")
+
+
+def main():
     event_handler = WatchDogHandler()
 
     observer = Observer()
-    observer.schedule(event_handler, watch_path, recursive=True)
+    observer.schedule(event_handler, content_path, recursive=True)
     observer.start()
 
+    build(content_path)
+
     handler = lambda *a, **kw: SimpleHTTPRequestHandler(
-        *a, directory=target, **kw
+        *a, directory="build", **kw
     )
 
     HTTPServer(("127.0.0.1", 8000), handler).serve_forever()
